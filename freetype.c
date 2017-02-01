3844444aea3531b8e43736968059196659e72128
djytw <djytw@live.com>
2017-02-01 19:20:04 +0800

    Add GUI Support

--- freetype.c
+++ freetype.c
@@ -2,32 +2,49 @@
 #include FT_FREETYPE_H
 #include "SDL2/SDL.h"
 #include <stdio.h>
-
+#include <string.h>
 FT_Library  library;
+unsigned char image[128][256];
+void draw_bitmap(FT_Bitmap *bitmap,int x, int y){
+  int i, j, p, q;
+  int x_max = x + bitmap->width;
+  int y_max = y + bitmap->rows;
+  for(i=x,p=0;i<x_max;i++,p++){
+    for (j=y,q=0;j<y_max;j++,q++){
+      if(i<0||j<0||i>=256||j>=128)continue;
+      image[j][i] |= bitmap->buffer[q * bitmap->width + p];
+    }
+  }
+}
+
 int main(){
    SDL_Init(SDL_INIT_VIDEO);
    SDL_Window *w = SDL_CreateWindow("ft test",0,0,1536,768, 0);
-   SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED);
+   SDL_Renderer *r = SDL_CreateRenderer(w, -1,SDL_RENDERER_ACCELERATED);
    SDL_Surface *s= SDL_CreateRGBSurface(0,1536,768,32,0,0,0,0);
    SDL_FillRect(s,NULL,0xFF888888);
    SDL_Rect re;re.w=5;re.h=5;
-   FT_Init_FreeType( &library );
+   FT_Init_FreeType(&library);
    FT_Face face;
-   FT_New_Face( library,"OpenSans-Regular.ttf",0,&face);
+   FT_New_Face(library,"OpenSans-Regular.ttf",0,&face);
    //FT_Set_Char_Size(face,0,16,3000,3000 );
    FT_Set_Pixel_Sizes(face,0,16);
-   FT_GlyphSlot  slot = face->glyph;
-   FT_Load_Char( face, 'g', FT_LOAD_RENDER);
+   FT_GlyphSlot slot=face->glyph;
    //FT_Render_Glyph( slot,FT_RENDER_MODE_MONO );
    int i,j;
-   for(i=0;i<*(&slot->bitmap.rows);i++){
-      for(j=0;j<*(&slot->bitmap.width);j++){
+   int penx=0,peny=16;
+   //generate
+   char str[]="Abgf01";
+   for(i=0;i<strlen(str);i++){
+      FT_Load_Char(face,str[i],FT_LOAD_RENDER);
+      draw_bitmap(&slot->bitmap,slot->bitmap_left+penx,peny-slot->bitmap_top);
+      penx += slot->advance.x >> 6;
+   }
+   //draw picture
+   for(i=0;i<128;i++)for(j=0;j<256;j++){
          re.x=j*6;re.y=i*6;
-         int color=255-(int)slot->bitmap.buffer[i**(&slot->bitmap.width)+j];
-         color=0xff000000+color*0x010101;
-         printf("draw alpha=%d color:#%08X x:%d y:%d\n",255-(int)slot->bitmap.buffer[i**(&slot->bitmap.width)+j],color,j,i);
+         int color=(255-image[i][j])*0x010101+0xff000000;
          SDL_FillRect(s,&re,color);
-      }
    }
    SDL_Texture *t = SDL_CreateTextureFromSurface(r, s);
    SDL_RenderCopy(r, t, NULL,NULL);
@@ -41,6 +58,9 @@ int main(){
              SDL_DestroyWindow(w);
              SDL_Quit();
              return 0;
+          }
+          if (e.type == SDL_MOUSEBUTTONDOWN){
+             printf("x:%d y:%d color:%02X\n",e.button.x/6,e.button.y/6,image[e.button.y/6][e.button.x/6]);
           }
       }
       SDL_Delay(10);
